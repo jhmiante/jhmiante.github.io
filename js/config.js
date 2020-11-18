2a4f8ca5bf9197999ed7fb739632f345d038e409
jhmiante <jhmiante@gmail.com>
2020-11-17 21:32:18 -0300

    config.js att

--- js/config.js
+++ js/config.js
@@ -1,3 +1,20 @@
+var htmlString = ""
 var pages = new Array()
+var namePages = new Array()
 
 pages[0] = "cs-web"
+namePages[0] = "Programacao Web"
+
+
+for (var i = 0; i < pages.length; i++){
+	htmlString += ' <div class="col-md-6 d-link text-center py-2 m-link" id="link-web"> ' +
+'							<strong class="m-2"> ' + namePages[i] + '</strong> ' +
+'							<img src=" https://jhmiante.github.io/img/'+ pages[i] +'.png" class="img-fluid f-link rounded mx-auto d-block my-2"> ' +
+'							<div class="d-flex justify-content-center"> ' +
+'								<a href="https://notedev.000webhostapp.com/' + pages[i] + '/" target="_blank" class="btn btn-info">Acessar</a> ' +
+'							</div>	 ' +
+'						</div> '
+}
+
+
+$("#paginas").html(htmlString)
