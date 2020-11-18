b68a318a1bb5768ac4992f443d8e5ce2aec11f30
jhmiante <jhmiante@gmail.com>
2020-11-17 21:41:10 -0300

    cs-poo

--- js/config.js
+++ js/config.js
@@ -3,11 +3,15 @@ var pages = new Array()
 var namePages = new Array()
 
 pages[0] = "cs-web"
-namePages[0] = "Programacao Web"
+namePages[0] = "Programação Web"
+
+
+pages[1] = "cs-poo"
+namePages[1] = "Programação POO"
 
 
 for (var i = 0; i < pages.length; i++){
-	htmlString += ' <div class="col-md-6 d-link text-center py-2 m-link" id="link-web"> ' +
+	htmlString += ' <div class="col-md-5 d-link text-center py-2 m-link" id="link-web"> ' +
 '							<strong class="m-2"> ' + namePages[i] + '</strong> ' +
 '							<img src=" https://jhmiante.github.io/img/'+ pages[i] +'.png" class="img-fluid f-link rounded mx-auto d-block my-2"> ' +
 '							<div class="d-flex justify-content-center"> ' +
@@ -17,4 +21,4 @@ for (var i = 0; i < pages.length; i++){
 }
 
 
-$("#paginas").html(htmlString)
+document.getElementById("paginas").innerHTML = htmlString
