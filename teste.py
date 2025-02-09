48971b41a05441b5eac11f28fd70dd1700db07d4
Jonathas Henrique <jhmiante@gmail.com>
2025-02-09 17:39:37 -0300

    Portfólio v2

--- teste.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-from pydark.date.dtget import getUtilDay
-                                        
-dia = datetime.now()
-dia_util = getUtilDay(dia, 5)
-
-print(dia_util)
